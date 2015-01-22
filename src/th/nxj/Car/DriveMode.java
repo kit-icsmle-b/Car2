779f79d2868476a80e18be6069def2622ef7b7ea
Takayuki Honma <hontakotako@gmail.com>
2015-01-22 16:22:36 +0900

    iroiro update.

--- src/th/nxj/Car/DriveMode.java
+++ src/th/nxj/Car/DriveMode.java
@@ -13,45 +13,45 @@ import lejos.util.Delay;
 /-------------------------------------------------------------------*/
 
 /**
- * Management class of running mode of the machine. 
+ * マシンの走行状態を管理するクラス 
  * @author BONN
  */
 public class DriveMode
 {
-	/** Black color ID */
+	/** 黒色のID */
 	public static final int COLOR_ID_BLACK = 7;
-	/** White color ID */
+	/** 白色のID */
 	public static final int COLOR_ID_WHITE = 6;
-	/** Green color ID */
+	/** 緑色のID */
 	public static final int COLOR_ID_GREEN = 1;
-	/** Red color ID */
+	/** 赤色のID */
 	public static final int COLOR_ID_RED = 0;
 	
-	/** Speed of normal running mode */
+	/** 通常走行モードのスピード */
 	public static final int NORMAL_SPEED_1 = 430;
 	public static final int NORMAL_SPEED_2 = 50;
 	
-	/** Speed of running on green mode */
+	/** 緑色通過時のスピード */
 	public static final int SLOW_SPEED_1 = 300;
 	public static final int SLOW_SPEED_2 = 10;
 	
-	/** Speed of Mobius( running into small circle )mode */
+	/** メビウスモード(小さい円の走行時)のスピード */
 	public static final int MOBIUS_SPEED_1 = 400;
 	public static final int MOBIUS_SPEED_2 = 170;
 	
-	/** Speed of line search mode */
+	/** サーチモード時のスピード */
 	public static final int SEARCH_SPEED_1_2 = 100;
 	
 	
-	private static int line_lost_time;
-	private static int color_state_prev;											// before color value
-	private static int white_scan_count = 0;										// 
-	private static int mobius_pass_flag = 0;										// Mobius mode passed flag
+	private static int line_lost_time;												// 黒線を見失い続けている時間
+	private static int color_state_prev;											// 直前に取得したカラーセンサ値
+	private static int white_scan_count = 0;										// メビウスモード時の旋回速度補正値
+	private static int mobius_pass_flag = 0;										// 小さい円通過を示すフラグ
 	WheelControl wheel = new WheelControl( NORMAL_SPEED_1 , NORMAL_SPEED_1 );		// タイヤの制御処理のインスタンス
 	LineSensor sensor = new LineSensor();											// カラーセンサーのインスタンス
 	
 	/**
-	 * constructor
+	 * コンストラクタ
 	 */
 	public DriveMode()
 	{
@@ -60,17 +60,17 @@ public class DriveMode
 	}
 	
 	/**
-	 * Normal running mode.
-	 * Scan black to Turn left,
-	 * Scan white to Turn right.
-	 * @return sstate The value of the running mode to be executed
-	 * 	when the next loop.
+	 * 通常走行モード
+	 * 黒検出で左回転、
+	 * 白検出で右回転する
+	 * @return sstate 　次のループ時に実行する走行モードの値
+	 * 
 	 */
 	public int InLineDrive()
 	{
 		
-		int sstate = 0;												 // Current running mode
-		int color = sensor.getState();								 // Latest color value
+		int sstate = 0;												 // 現在の走行モード
+		int color = sensor.getState();								 // 直前のカラーセンサ値
 		
 		if( color_state_prev != color )								 // 最新のセンサ値と直前の値が異なっていたら
 		{
@@ -79,11 +79,11 @@ public class DriveMode
 			case COLOR_ID_BLACK:
 				wheel.setSpeed( NORMAL_SPEED_2 , NORMAL_SPEED_1 );
 				wheel.forward();
-				line_lost_time = 0;									 // 黒線が検出されたので、ロストしている時間を0に戻す
+				//line_lost_time = 0;									 // 黒線が検出されたので、ロストしている時間を0に戻す
 				break;
 			case COLOR_ID_WHITE:
 				if( mobius_pass_flag == 0 )
-					wheel.setSpeed( NORMAL_SPEED_1 , NORMAL_SPEED_2 + 60 );
+					wheel.setSpeed( NORMAL_SPEED_1 , NORMAL_SPEED_2 + 40 );
 				else
 					wheel.setSpeed( NORMAL_SPEED_1 , NORMAL_SPEED_2 + 250 );
 				wheel.forward();
@@ -94,7 +94,7 @@ public class DriveMode
 				break;
 			case COLOR_ID_RED:
 				wheel.stop();										 // 赤色はゴールなので停止
-				line_lost_time = 0;
+				//line_lost_time = 0;
 				white_scan_count = 0;
 				mobius_pass_flag = 0;
 			}
@@ -102,8 +102,10 @@ public class DriveMode
 		}
 		if( color_state_prev == COLOR_ID_WHITE )
 			line_lost_time++;
+		else
+			line_lost_time = 0;
 		
-		if( line_lost_time > /*5000*/10000 )									 // 黒線を見失っている時間が5000カウントを超えたら
+		if( line_lost_time > 10000 )						 		 // 黒線を見失っている時間が10000カウントを超えたら
 			sstate = 1;												 // サーチモード(Car.javaを参照)に切り替えを指示
 		
 		return sstate;
@@ -156,7 +158,7 @@ public class DriveMode
 			color = sensor.getState();
 			if( color_state_prev != color )
 			{
-				switch( color )										// センサーから色情報を取得
+				switch( color )
 				{
 				case COLOR_ID_BLACK:
 					mobius_pass_flag = 1;
@@ -188,16 +190,17 @@ public class DriveMode
 		
 		if( color_state_prev != color )
 		{
-			switch( color )											// カラーセンサから色情報を取得
+			switch( color )
 			{
 			case COLOR_ID_BLACK:
 				wheel.setSpeed( MOBIUS_SPEED_1 + white_scan_count , MOBIUS_SPEED_2 );
 				wheel.forward();
-				line_lost_time = 0;
+				//line_lost_time = 0;
 				break;
 			case COLOR_ID_WHITE:
 				wheel.setSpeed( MOBIUS_SPEED_2 , MOBIUS_SPEED_1 );
 				wheel.forward();
+				if( white_scan_count < 100 )
 				white_scan_count+=25;
 				break;
 			case COLOR_ID_GREEN:
@@ -206,14 +209,16 @@ public class DriveMode
 				white_scan_count = 0;
 				break;
 			case COLOR_ID_RED:
-				wheel.stop();										// emergency stop.
+				wheel.stop();										// 緊急停止
 			}
 			color_state_prev = color;
 		}
 		if( color_state_prev == COLOR_ID_WHITE )
 			line_lost_time++;
+		else
+			line_lost_time = 0;
 		
-		if( line_lost_time > 5000 )
+		if( line_lost_time > 10000 )
 			sstate = 1;
 		
 		return sstate;
@@ -229,10 +234,10 @@ public class DriveMode
 	public int SearchLine()
 	{
 		int sstate = 0;
-		
+		int color = sensor.getState();
 		wheel.setSpeed( SEARCH_SPEED_1_2 , SEARCH_SPEED_1_2 );
 		
-		if( sensor.getState() != COLOR_ID_BLACK )				// 黒線を見つけるまで継続
+		if( color == COLOR_ID_WHITE )				// 黒線を見つけるまで継続
 		{
 			wheel.TurnRight();
 			Delay.msDelay( 200 );
@@ -240,7 +245,9 @@ public class DriveMode
 			Delay.msDelay( 500 );
 			sstate = 1;
 		}
-		else
+		else if( color == COLOR_ID_RED )
+			wheel.stop();							// 緊急停止
+		else if( color == COLOR_ID_BLACK )
 		{
 			sstate = 0;
 			wheel.setSpeed( 100 , 100 );
